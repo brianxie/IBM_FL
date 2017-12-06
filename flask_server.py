436bfd13d9b821f0be29c58976f00806be69b7b8
Brian Xie <briancxie@gmail.com>
2017-12-05 23:38:23 -0800

    made code actually take in arguments lol

--- flask_server.py
+++ flask_server.py
@@ -16,12 +16,13 @@ API_KEY = "41531189cb11becf0e0baec61ab2fda35e5130f6"
 def root():
     images_file = request.data.decode('utf-8')
     json_dict = json.loads(images_file)
-    print(json_dict["images_file"])
-    visual_recognition = VisualRecognitionV3('2016-05-20', api_key=API_KEY)
-    # print(json.dumps(visual_recognition.detect_faces(images_url="file:///Users/bcx/Desktop/files/cal/17-2/recruiting/ibm/IBM_FL/prez.jpg")))
-    print(json.dumps(visual_recognition.detect_faces(images_url="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/President_Barack_Obama.jpg/615px-President_Barack_Obama.jpg"), indent=2))
-    return images_file
+    file = json_dict["images_file"]
+    print(file)
+    json_response = json.dumps(visual_recognition.detect_faces(images_url=file), indent=2)
+    print(json_response)
+    return json_response
 
 if __name__ == "__main__":
     # app.run(host="0.0.0.0")
+    visual_recognition = VisualRecognitionV3('2016-05-20', api_key=API_KEY)
     app.run()
--- frontend.js
+++ frontend.js
@@ -1,46 +1,31 @@
 console.log("hi this isn't completely broken");
 
-function send_file_name() {
+function send_file_name(file_name) {
     url = "http://127.0.0.1:5000"
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
-    data = {};
-    data["images_file"] = "prez.jpg";
+    var data = {};
+    data["images_file"] = file_name;
     xhr.send(JSON.stringify(data));
+
+    var response_json = "";
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            response_json = xhr.responseText;
+            var identity = extract_identity(response_json);
+            console.log(identity);
+        }
+    }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function send_image(image_path) {
-//     var dest_path = "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/detect_faces?api_key={41531189cb11becf0e0baec61ab2fda35e5130f6}&version=2016-05-20"
-//     var params = "{images_file: './prez.jpg'}"
-//     post(dest_path)
-// }
-
-// function post(url) {
-//     var xhr = new XMLHttpRequest();
-//     xhr.open("POST", url, true);
-//     xhr.setRequestHeader('Content-Type', 'application/json');
-//     xhr.send(JSON.stringify({
-//         images_file: "prez.jpg"
-//     }));
-// }
-
-// function extract_identity(json_text) {
-//     images = JSON.parse(json_text).images;
-// }
+function extract_identity(json_text) {
+    var obj = JSON.parse(json_text);
+    image = obj.images[0];
+    face = image.faces[0];
+    identity = face.identity;
+    name = identity.name;
+    return name;
+}
